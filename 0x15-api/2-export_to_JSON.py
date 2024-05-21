21bf2b13a7a6f4676576fe8af816b9f3dc8fe153
007Luciano <lucky.h.fatch@gmail.com>
2024-05-21 05:03:00 -0700

    lis of dictionaries

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-""" Export API data to JSON """
+""" Python to get data from an API and convert to Json"""
+import csv
 import json
 import requests
 import sys
 
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
     USER_ID = sys.argv[1]
-    try:
-        int(USER_ID)
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-
-    url_to_user = f'https://jsonplaceholder.typicode.com/users/{USER_ID}'
+    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
     res = requests.get(url_to_user)
-
-    if res.status_code != 200:
-        print("Error: Employee not found.")
-        sys.exit(1)
-
+    """Documentation"""
     USERNAME = res.json().get('username')
-    url_to_task = f'{url_to_user}/todos'
+    """Documentation"""
+    url_to_task = url_to_user + '/todos'
     res = requests.get(url_to_task)
     tasks = res.json()
 
@@ -33,13 +22,9 @@ if __name__ == '__main__':
         TASK_COMPLETED_STATUS = task.get('completed')
         TASK_TITLE = task.get('title')
         dict_data[USER_ID].append({
-            "task": TASK_TITLE,
-            "completed": TASK_COMPLETED_STATUS,
-            "username": USERNAME
-        })
-
-    json_file_name = f'{USER_ID}.json'
-    with open(json_file_name, 'w') as f:
-        json.dump(dict_data, f, indent=4)
-
-    print(f"Data for employee ID {USER_ID} has been written to {json_file_name}")
+                                  "task": TASK_TITLE,
+                                  "completed": TASK_COMPLETED_STATUS,
+                                  "username": USERNAME})
+    """print(dict_data)"""
+    with open('{}.json'.format(USER_ID), 'w') as f:
+        json.dump(dict_data, f)
