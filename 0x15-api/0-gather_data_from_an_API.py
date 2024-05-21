28adba536b4338810c039138aa788466d08ecc50
007Luciano <lucky.h.fatch@gmail.com>
2024-05-21 06:13:01 -0700

    api fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""
-Gather employee data from API
-"""
+'''
+gather employee data from API
+'''
 
+import re
 import requests
 import sys
 
@@ -10,27 +11,20 @@ REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        try:
+        if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-        except ValueError:
-            print("Employee ID must be an integer.")
-            sys.exit(1)
-
-        user_response = requests.get(f'{REST_API}/users/{id}')
-        if user_response.status_code != 200:
-            print("Error: Employee not found.")
-            sys.exit(1)
-
-        user_data = user_response.json()
-        emp_name = user_data.get('name')
-
-        todos_response = requests.get(f'{REST_API}/todos?userId={id}')
-        todos_data = todos_response.json()
-
-        tasks = list(filter(lambda x: x.get('userId') == id, todos_data))
-        completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-
-        for task in completed_tasks:
-            print(f'\t {task.get("title")}')
-    else:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
