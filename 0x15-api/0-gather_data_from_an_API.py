9012aa5b388eeda22ecb965514237ec2c46aefb2
007Luciano <lucky.h.fatch@gmail.com>
2024-05-21 04:45:26 -0700

    export csv

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,25 +15,25 @@ if __name__ == '__main__':
         except ValueError:
             print("Employee ID must be an integer.")
             sys.exit(1)
-        
+
         user_response = requests.get(f'{REST_API}/users/{id}')
         if user_response.status_code != 200:
             print("Error: Employee not found.")
             sys.exit(1)
-        
+
         user_data = user_response.json()
         emp_name = user_data.get('name')
-        
+
         todos_response = requests.get(f'{REST_API}/todos?userId={id}')
         todos_data = todos_response.json()
-        
+
         tasks = list(filter(lambda x: x.get('userId') == id, todos_data))
         completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-        
+
         print(
             f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):'
         )
-        
+
         for task in completed_tasks:
             print(f'\t {task.get("title")}')
     else:
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,30 +8,30 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
-    
+
     user_id = sys.argv[1]
-    
+
     try:
         user_id_int = int(user_id)
     except ValueError:
         print("Employee ID must be an integer.")
         sys.exit(1)
-    
+
     # Fetch user information
     user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
         print("Error: Employee not found.")
         sys.exit(1)
-    
+
     user_data = user_response.json()
     user_name = user_data.get('username')
-    
+
     # Fetch user's TODO list
     todos_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
-    
+
     # Write to CSV
     csv_file_name = f'{user_id}.csv'
     with open(csv_file_name, mode='w', newline='') as csvfile:
@@ -40,5 +40,3 @@ if __name__ == '__main__':
             completed = task.get('completed')
             title_task = task.get('title')
             csv_writer.writerow([user_id, user_name, completed, title_task])
-    
-    print(f"Data for employee ID {user_id} has been written to {csv_file_name}")
